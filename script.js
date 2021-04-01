ff9fe913ffb5e35c7e859f23d867152e089eba75
Diego Melo <robsonribeiroely@hotmail.com>
2021-04-01 14:45:05 -0300

    Versão 1.2.0
    Foi criada a pasta script.js e os script do slider

---
+++ script.js
@@ -0,0 +1,34 @@
+
+/********** Código do slider header *********/
+let time = 5000,
+    currentImageIndex = 0,
+    images = document
+        .querySelectorAll("#slider img")
+max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if (currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
+
+/****************************  Final do slider          *************** */
+
+
